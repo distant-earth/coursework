3647b555e49a3d402318919dfee4f58e412bf0c0
distant-earth <st068189@student.spbu.ru>
2022-05-11 07:53:42 +0300

    edited and cleaned for use

--- fortran_DP/modd.f90
+++ fortran_DP/modd.f90
@@ -7,8 +7,8 @@ subroutine func_f(N, t, y, res, RPAR, IPAR)
 implicit none
 integer N, IPAR
 real(8) t, y(N), res(N), a, ecc, omega, f, n_mean, RPAR, mass
-real(8), parameter :: G = 0.03768444632476504 ! au3 / (M_jup yr2)
-! 39.476926408897626 ! au3 / (M_sun yr2)
+real(8), parameter :: G = 39.476926408897626 ! au3 / (M_sun yr2)
+! 0.03768444632476504 ! au3 / (M_jup yr2)
 ! 39476926408897.63d0 ! au3 / (M_sun Myr2)
 ! 3.9476926408897634e+19 ! au3 / (M_sun Gyr2)
     a = y(1)
@@ -17,7 +17,7 @@ real(8), parameter :: G = 0.03768444632476504 ! au3 / (M_jup yr2)
     f = y(4)
     mass = y(5)
     n_mean= sqrt(G * mass / a**3)
-    res(5) = -0.000001d0
+    res(5) = -1e-9
     res(1) = a * ((1 + ecc**2 + 2 * ecc * cos(f)) / (ecc**2 - 1)) * (res(5) / mass)
     res(2) = - (ecc + cos(f)) * (res(5) / mass)
     res(3) = - (sin(f) / ecc) * (res(5) / mass)
@@ -30,7 +30,8 @@ subroutine func_E(N, t, y, res, RPAR, IPAR)
 implicit none
 integer N, IPAR
 real(8) t, y(N), res(N), a, ecc, omega, E, n_mean, RPAR, mass
-real(8), parameter :: G = 0.03768444632476504 ! au3 / (M_jup yr2)
+real(8), parameter :: G = 39.476926408897626 ! au3 / (M_sun yr2)
+! 0.03768444632476504 ! au3 / (M_jup yr2)
 ! 39.476926408897626 ! au3 / (M_sun yr2)
 ! 39476926408897.63d0 ! au3 / (M_sun Myr2)
 ! 3.9476926408897634e+19 ! au3 / (M_sun Gyr2)
@@ -40,7 +41,7 @@ real(8), parameter :: G = 0.03768444632476504 ! au3 / (M_jup yr2)
     E = y(4)
     mass = y(5)
     n_mean= sqrt(G * mass / a**3)
-    res(5) = -0.000001d0
+    res(5) = -1e-9
     res(1) = - a * ((1 + ecc * cos(E)) / (1 - ecc * cos(E))) * (res(5) / mass)
     res(2) = ((1 - ecc**2) * cos(E) / (ecc * cos(E) - 1)) * (res(5) / mass)
     res(3) = (sqrt(1 - ecc**2) * sin(E) / (ecc * (ecc * cos(E) - 1))) * (res(5) / mass)
@@ -48,12 +49,12 @@ real(8), parameter :: G = 0.03768444632476504 ! au3 / (M_jup yr2)
 end subroutine func_E
 
 
+! Подпрограмма для вывода промежуточных значений в файл:
 subroutine solout(NR, t_old, t, y, N, con, icomp, ND, RPAR, IPAR, IRTRN)
 integer IPAR, NR, N, IRTRN, ND
 real(8) RPAR, t_old, t, y(N), con, icomp
 
-!if (mod(NR, 100) == 0) 
-write(IPAR, *) NR, t, y
+if (mod(NR, 100) == 0) write(IPAR, *) NR, t, y
 
 end subroutine solout
 
--- fortran_DP/plotting.py
+++ fortran_DP/plotting.py
@@ -15,7 +15,7 @@ with open('f_RESULT') as table:
         omega1.append(float(line.split()[4]))
         f.append(float(line.split()[5]))
         mass1.append(float(line.split()[6]))
-
+        
 t2 = []
 a2 = []
 ecc2 = []
@@ -29,73 +29,89 @@ with open('E_RESULT') as table:
         omega2.append(float(line.split()[4]))
         mass2.append(float(line.split()[6]))
 
-step = 1                        # шаг, с которым из результата берутся точки для графика
+step = 100    
+# Шаг, с которым из выведенных в файл точек берутся точки для графика.
 
-plt.title("Большая полуось") # (теоретически)")
+plt.title("Большая полуось")
 plt.xlabel('t [лет]')
 plt.ylabel('a [а.е.]')
 plt.plot(t1[::step], a1[::step], 'r-', label = 'Через f')
 plt.plot(t2[::step], a2[::step], 'b--', label = 'Через E')
+
+# Для вывода теоретической кривой:
 #def func(x):
-	#y = [0.006 / (1 - 0.00001 * i) for i in x] 
-	#return y
-#plt.plot(t1[::step], func(t1[::step]), 'k-', label = 'a(t) = C / m(t)')
-plt.legend(loc = 'upper left', framealpha = 1) #title = fr'$a_0 = {}$')
+#	y = [0.0747175 / (0.6175 - 1e-9 * i) for i in x] 
+#	return y
+#plt.plot(t1[::step], func(t1[::step]), 'k-', label = r'$a(t) = \frac{M_0 \cdot a_0}{M(t)}$')
+
+# Для изменения формата чисел вдоль оси ординат:
+#current_values = plt.gca().get_yticks()
+#plt.gca().set_yticks(current_values)
+#plt.gca().set_yticklabels(['{:.11f}'.format(x) for x in current_values])
+
+plt.legend(loc = 'upper left', framealpha = 1)
 plt.tight_layout()
 
 plt.figure()
 plt.title("Эксцентриситет")
 plt.xlabel('t [лет]')
 plt.ylabel('e')
-plt.gca().set_ylim([0, 1])
+plt.gca().set_ylim([0.70, 0.72])
 plt.plot(t1[::step], ecc1[::step], 'r-', label = 'Через f')
 plt.plot(t2[::step], ecc2[::step], 'b--', label = 'Через E')
-plt.legend(loc = 'upper left', framealpha = 1) # title = fr'$e_0 = {ecc0}$')
+plt.legend(loc = 'upper left', framealpha = 1)
 plt.tight_layout()
 
 plt.figure()
 plt.title("Аргумент перицентра")
 plt.xlabel('t [лет]')
 plt.ylabel(r'$\omega$ [рад]')
-#plt.gca().set_ylim([0.1, 0.3])
+plt.gca().set_ylim([4.12, 4.18])
 plt.plot(t1[::step], omega1[::step], 'r-', label = 'Через f')
 plt.plot(t2[::step], omega2[::step], 'b--', label = 'Через E')
-plt.legend(loc = 'upper left', framealpha = 1) #title = fr'$\omega_0 = {round(omega0, 3)}$')
+plt.legend(loc = 'upper left', framealpha = 1)
 plt.tight_layout()
 
 g1 = np.array(a1) * (1 - np.array(ecc1))
 g2 = np.array(a2) * (1 - np.array(ecc2))
 
 plt.figure()
-plt.title("Перицентрич. расстояние g = a(1 - e)")
+plt.title("Перицентрич. расстояние q = a(1 - e)")
 plt.xlabel('t [лет]')
-plt.ylabel('g [а.е.]')
+plt.ylabel('q [а.е.]')
 plt.plot(t1[::step], g1[::step], 'r-', label = 'Через f')
 plt.plot(t2[::step], g2[::step], 'b--', label = 'Через E')
-plt.legend(loc = 'upper left', framealpha = 1) #title = fr'$\omega_0 = {round(omega0, 3)}$')
+#current_values = plt.gca().get_yticks()
+#plt.gca().set_yticks(current_values)
+#plt.gca().set_yticklabels(['{:.11f}'.format(x) for x in current_values])
+plt.legend(loc = 'upper left', framealpha = 1)
 plt.tight_layout()
+
 
 ma1 = np.array(mass1) * np.array(a1)
 ma2 = np.array(mass2) * np.array(a2)
 
 plt.figure()
-plt.title("Ma = const")
+plt.title(r"Ma $\approx$ Ma(1 - e$^2$) = const")
 plt.xlabel('t [лет]')
-plt.ylabel('M$\cdot$a [$M_{системы}\cdot$ a.e.]')
-#plt.gca().set_ylim([0.004, 0.008])
+plt.ylabel('M$\cdot$a [$M_{Солнца}\cdot$ a.e.]')
+plt.gca().set_ylim([0.07, 0.08])
 plt.plot(t1[::step], ma1[::step], 'r-', label = 'Через f')
 plt.plot(t2[::step], ma2[::step], 'b--', label = 'Через E')
-plt.legend(loc = 'upper left', framealpha = 1) #title = fr'$\omega_0 = {round(omega0, 3)}$')
+plt.legend(loc = 'upper left', framealpha = 1)
 plt.tight_layout()
 
 plt.figure()
 plt.title("Изменение массы")
 plt.xlabel('t [лет]')
-plt.ylabel('M [$M_{системы}$]')
-#plt.gca().set_ylim([0.6174, 0.6176])
+plt.ylabel('M [$M_{Солнца}$]')
+plt.gca().set_ylim([0.58, 0.63])
 plt.plot(t1[::step], mass1[::step], 'r-', label = 'Через f')
 plt.plot(t2[::step], mass2[::step], 'b--', label = 'Через E')
-plt.legend(loc = 'lower left', framealpha = 1) #title = fr'$\omega_0 = {round(omega0, 3)}$')
+#current_values = plt.gca().get_yticks()
+#plt.gca().set_yticks(current_values)
+#plt.gca().set_yticklabels(['{:.10f}'.format(x) for x in current_values])
+plt.legend(loc = 'lower left', framealpha = 1)
 plt.tight_layout()
 
 plt.show()
